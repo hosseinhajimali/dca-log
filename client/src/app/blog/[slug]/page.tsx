c51143babb7a29dcf9f538567c6b4ee49e5cae8b
Hossein <hossein.hajimali@gmail.com>
2026-05-18 14:29:05 +0200

    blog post info after share

--- client/src/app/blog/[slug]/page.tsx
+++ client/src/app/blog/[slug]/page.tsx
@@ -6,7 +6,26 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
   const { slug } = await params;
   const post = getBlogPost(slug);
   if (!post) return { title: 'Blog | DCAlog' };
-  return { title: `${post.title} | DCAlog` };
+
+  const url = `https://dcalog.com/blog/${slug}`;
+
+  return {
+    title: `${post.title} | DCAlog`,
+    description: post.excerpt,
+    openGraph: {
+      title: post.title,
+      description: post.excerpt,
+      url,
+      type: 'article',
+      publishedTime: post.date,
+      siteName: 'DCAlog',
+    },
+    twitter: {
+      card: 'summary',
+      title: post.title,
+      description: post.excerpt,
+    },
+  };
 }
 
 export default BlogPost;
